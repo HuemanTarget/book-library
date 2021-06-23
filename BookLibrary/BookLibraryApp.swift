1c1702e6ae89ab63ce102de8d69704b1b19d5884
HuemanTarget <joshbasche@live.com>
2021-06-23 17:53:24 -0500

    first commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Joshua Basche on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BookLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
